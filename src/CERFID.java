dc103ba987729bbb2e8bf01929f8cb1263e76644
Marcio-Manrique <u202415879@upc.edu.pe>
2024-11-16 12:53:30 -0500

    Marcio Manrique - Actualización creación de Ejecutador

---
+++ src/CERFID.java
@@ -0,0 +1,5 @@
+public class CERFID {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Testing.java
+++
@@ -1,8 +0,0 @@
-public class Testing {
-    public static void main(String[] args) {
-        System.out.println("asdfasdfads");
-        System.out.println("Hola");
-        System.out.println("QUe tal");
-        System.out.println("123");
-    }
-}
