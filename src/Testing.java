c28c0ec95d992b9485a370670448415f24388346
Marcio-Manrique <u202415879@upc.edu.pe>
2024-11-16 12:37:46 -0500

    git fecth

--- src/Testing.java
+++ src/Testing.java
@@ -1,5 +1,7 @@
 public class Testing {
     public static void main(String[] args) {
         System.out.println("asdfasdfads");
+        System.out.println("Hola");
+        System.out.println("QUe tal");
     }
 }
