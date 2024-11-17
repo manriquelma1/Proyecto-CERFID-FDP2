e18d75dc526410a1c04d7c67a76f2413e80d5433
Marcio-Manrique <u202415879@upc.edu.pe>
2024-11-17 14:36:24 -0500

    Marcio Manrique
     ==Actualizaciones==
     Class CERFID:
     -Creación de arreglo para almacenar los pacientes y terapeutas registrados.
    
     -Creación de constructor y agregar la lista creada al mismo.
    
     -Creación del metodo imprimirListaTrabajadores que va listar (Esta lista solo ha sido creada para hacer pruebas, luego se eliminiara).

--- src/CERFID/CERFID.java
+++ src/CERFID/CERFID.java
@@ -1,12 +1,22 @@
 package CERFID;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CERFID {
+    private List<Personas> listaPersonasRegistradas;
 
-    private List<Personas> listaPersonas;
+    public CERFID() {
+        this.listaPersonasRegistradas = new ArrayList<>();
+    }
 
+    public void agregarpersonas(Personas personas) {
+        this.listaPersonasRegistradas.add(personas);
+    }
 
-
-
+    public void imprimirListaTrabajadores() {
+        for (Personas personas : listaPersonasRegistradas) {
+            System.out.println(personas);
+        }
+    }
 }
--- src/CERFID/Main.java
+++ src/CERFID/Main.java
@@ -1,7 +1,60 @@
 package CERFID;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        CERFID addterapeuta = new CERFID();
+        int opcion = -1;
+
+        while (opcion != 0) {
+            System.out.println("\n===== Menu Principal =====");
+            System.out.println("1. Registrar Terapeuta");
+            System.out.println("2. Registrar Paciente");
+            System.out.println("3. Programar Cita");
+            System.out.println("4. Verificar Historia Clinica");
+            System.out.println("5. Reporte de Citas");
+            System.out.println("0. Salir");
+            System.out.print("\n seleccionar opción : ");
+
+            if (scan.hasNextInt()) {
+                opcion = scan.nextInt();
+                switch (opcion) {
+                    case 1:
+                        System.out.println("DNI del Terapeuta: ");
+                        String numDNI = scan.next();
+                        System.out.println("Nombre del Terapeuta: ");
+                        String nombre = scan.next();
+                        System.out.println("Apellido del Terapeuta: ");
+                        String apellidos = scan.next();
+                        System.out.println("Codigo de Trabaajdor: ");
+                        String codigoTrabajador = scan.next();
+                        addterapeuta.agregarpersonas(new Terapeutas(numDNI,nombre,apellidos,codigoTrabajador));
+                        // Add logic to handle adding an item
+                        break;
+                    case 2:
+                        System.out.println("You chose to view items.");
+                        // Add logic to handle viewing items
+                        break;
+                    case 3:
+                        System.out.println("You chose to delete an item.");
+                        // Add logic to handle deleting an item
+                        break;
+                    case 0:
+                        System.out.println("Exiting the program. Goodbye!");
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+            }
+        }
+        addterapeuta.imprimirListaTrabajadores();
 
     }
+
 }
