dbb1064d22778a24005b20c4b79b9de3653a9460
Marcio-Manrique <u202415879@upc.edu.pe>
2024-11-17 09:45:52 -0500

    Agregue las classes Main (Ejecutador), Personas, Pacientes y Terapeutas a mi branch- actualizacion 2

--- src/CERFID/Paciente.java
+++ src/CERFID/Paciente.java
@@ -1,5 +1,11 @@
 package CERFID;
 
 class Paciente extends Personas {
+    private int noSesiones;
+    private String prescripcionMedica;
+
+    public Paciente(String numDNI, String nombre, String apellido) {
+        super(numDNI, nombre, apellido);
+    }
 
 }
--- src/CERFID/Terapeutas.java
+++ src/CERFID/Terapeutas.java
@@ -1,4 +1,9 @@
 package CERFID;
 
 class Terapeutas extends Personas {
+    private String codigoTrabajador;
+
+    public Terapeutas(String numDNI, String nombre, String apellido) {
+        super(numDNI, nombre, apellido);
+    }
 }
